de008ebadeb156d4627f8af765883ed37a7e78f3
Graham Hoyes <accounts@grahamhoyes.com>
2024-08-11 10:26:41 -0400

    Add flow command to filter streams by language

---
+++ FlowPlugins/CommunityFlowPlugins/ffmpegCommand/ffmpegCommandFilterStreamByLanguage/1.0.0/index.js
@@ -0,0 +1,128 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.plugin = exports.details = void 0;
+var flowUtils_1 = require("../../../../FlowHelpers/1.0.0/interfaces/flowUtils");
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+var details = function () { return ({
+    name: 'Filter Stream by Language',
+    description: 'Filter streams by language, keeping at least one stream for each type. '
+        + 'Recommended to follow this with a reorder streams plugin.',
+    style: {
+        borderColor: '#6efefc',
+    },
+    tags: 'video,audio',
+    isStartPlugin: false,
+    pType: '',
+    requiresVersion: '2.11.01',
+    sidebarPosition: -1,
+    icon: '',
+    inputs: [
+        {
+            label: 'Languages',
+            name: 'languages',
+            type: 'string',
+            defaultValue: 'eng',
+            inputUI: {
+                type: 'text',
+            },
+            tooltip: "Specify the language tags order, separated by commas.\n        \\nExample:\\n\n        eng,fre,und\n        ",
+        },
+    ],
+    outputs: [
+        {
+            number: 1,
+            tooltip: 'Continue to next plugin',
+        },
+    ],
+}); };
+exports.details = details;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var plugin = function (args) {
+    var _a;
+    var lib = require('../../../../../methods/lib')();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
+    args.inputs = lib.loadDefaultValues(args.inputs, details);
+    (0, flowUtils_1.checkFfmpegCommandInit)(args);
+    var originalStreams = JSON.parse(JSON.stringify(args.variables.ffmpegCommand.streams));
+    var languages = args.inputs.languages.split(',');
+    // A nested map to let us process the entire file in a single pass.
+    // The outer map is keyed by codec type (video, audio, etc.). The inner map is
+    // a map from language code to overall stream index.
+    // Later logic depends on the fact that map iteration is by insertion order,
+    // which is how stream types get inserted back in the original order (as long
+    // as they aren't interleaved).
+    var streamTypeLanguageIndexes = new Map();
+    originalStreams.forEach(function (stream, i) {
+        var _a, _b;
+        var streamType = stream.codec_type;
+        if (!streamTypeLanguageIndexes.has(streamType)) {
+            streamTypeLanguageIndexes.set(streamType, new Map());
+        }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        var languageIndexes = streamTypeLanguageIndexes.get(streamType);
+        var language = ((_a = stream.tags) === null || _a === void 0 ? void 0 : _a.language) || 'und';
+        if (!languageIndexes.has(language)) {
+            languageIndexes.set(language, []);
+        }
+        (_b = languageIndexes.get(language)) === null || _b === void 0 ? void 0 : _b.push(i);
+    });
+    // Map from stream type to streams
+    var streamsToKeep = new Map();
+    var doProcessing = false;
+    streamTypeLanguageIndexes.forEach(function (langIndexes, streamType) {
+        // All the streams to keep for this stream type, in the preferred order
+        var filteredStreams = languages
+            .map(function (lang) { return (langIndexes.get(lang) || []).map(function (i) { return originalStreams[i]; }); })
+            .flat();
+        if (filteredStreams.length === 0) {
+            // If no matching streams were found, keep the originals
+            streamsToKeep.set(streamType, originalStreams.filter(function (s) { return s.codec_type === streamType; }));
+            args.jobLog("No matching streams were found for codec type ".concat(streamType, ", keeping the originals."));
+        }
+        else {
+            streamsToKeep.set(streamType, filteredStreams);
+            doProcessing = true;
+        }
+    });
+    if (doProcessing) {
+        // eslint-disable-next-line no-param-reassign
+        args.variables.ffmpegCommand.shouldProcess = true;
+        var outputStreams = Array.from(streamsToKeep)
+            .map(function (_a) {
+            var streams = _a[1];
+            return streams;
+        })
+            .flat();
+        // eslint-disable-next-line no-param-reassign
+        args.variables.ffmpegCommand.streams = outputStreams;
+        // Set the first stream for each codec type as the default, clearing the rest
+        var seenStreamTypes_1 = new Set();
+        var dispositionRemovalArgs_1 = [];
+        var dispositionSetArgs_1 = [];
+        outputStreams.forEach(function (stream, i) {
+            if (seenStreamTypes_1.has(stream.codec_type)) {
+                dispositionRemovalArgs_1.push("-disposition:".concat(i), '0');
+            }
+            else {
+                dispositionSetArgs_1.push("-disposition:".concat(i), 'default');
+                seenStreamTypes_1.add(stream.codec_type);
+            }
+        });
+        (_a = args.variables.ffmpegCommand.overallOuputArguments).push.apply(_a, __spreadArray(__spreadArray([], dispositionRemovalArgs_1, false), dispositionSetArgs_1, false));
+    }
+    return {
+        outputFileObj: args.inputFileObj,
+        outputNumber: 1,
+        variables: args.variables,
+    };
+};
+exports.plugin = plugin;
---
+++ FlowPluginsTs/CommunityFlowPlugins/ffmpegCommand/ffmpegCommandFilterStreamByLanguage/1.0.0/index.ts
@@ -0,0 +1,151 @@
+import { checkFfmpegCommandInit } from '../../../../FlowHelpers/1.0.0/interfaces/flowUtils';
+import {
+  IffmpegCommandStream,
+  IpluginDetails,
+  IpluginInputArgs,
+  IpluginOutputArgs,
+} from '../../../../FlowHelpers/1.0.0/interfaces/interfaces';
+
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+const details = (): IpluginDetails => ({
+  name: 'Filter Stream by Language',
+  description:
+    'Filter streams by language, keeping at least one stream for each type. '
+    + 'Recommended to follow this with a reorder streams plugin.',
+  style: {
+    borderColor: '#6efefc',
+  },
+  tags: 'video,audio',
+  isStartPlugin: false,
+  pType: '',
+  requiresVersion: '2.11.01',
+  sidebarPosition: -1,
+  icon: '',
+  inputs: [
+    {
+      label: 'Languages',
+      name: 'languages',
+      type: 'string',
+      defaultValue: 'eng',
+      inputUI: {
+        type: 'text',
+      },
+      tooltip: `Specify the language tags order, separated by commas.
+        \\nExample:\\n
+        eng,fre,und
+        `,
+    },
+  ],
+  outputs: [
+    {
+      number: 1,
+      tooltip: 'Continue to next plugin',
+    },
+  ],
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const plugin = (args: IpluginInputArgs): IpluginOutputArgs => {
+  const lib = require('../../../../../methods/lib')();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
+  args.inputs = lib.loadDefaultValues(args.inputs, details);
+
+  checkFfmpegCommandInit(args);
+
+  const originalStreams: IffmpegCommandStream[] = JSON.parse(
+    JSON.stringify(args.variables.ffmpegCommand.streams),
+  );
+
+  const languages = (args.inputs.languages as string).split(',');
+
+  // A nested map to let us process the entire file in a single pass.
+  // The outer map is keyed by codec type (video, audio, etc.). The inner map is
+  // a map from language code to overall stream index.
+  // Later logic depends on the fact that map iteration is by insertion order,
+  // which is how stream types get inserted back in the original order (as long
+  // as they aren't interleaved).
+  const streamTypeLanguageIndexes: Map<
+    string,
+    Map<string, number[]>
+  > = new Map();
+
+  originalStreams.forEach((stream, i) => {
+    const streamType = stream.codec_type;
+
+    if (!streamTypeLanguageIndexes.has(streamType)) {
+      streamTypeLanguageIndexes.set(streamType, new Map());
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const languageIndexes = streamTypeLanguageIndexes.get(streamType)!;
+
+    const language = stream.tags?.language || 'und';
+    if (!languageIndexes.has(language)) {
+      languageIndexes.set(language, []);
+    }
+
+    languageIndexes.get(language)?.push(i);
+  });
+
+  // Map from stream type to streams
+  const streamsToKeep: Map<string, IffmpegCommandStream[]> = new Map();
+  let doProcessing = false;
+
+  streamTypeLanguageIndexes.forEach((langIndexes, streamType) => {
+    // All the streams to keep for this stream type, in the preferred order
+    const filteredStreams = languages
+      .map((lang) => (langIndexes.get(lang) || []).map((i) => originalStreams[i]))
+      .flat();
+
+    if (filteredStreams.length === 0) {
+      // If no matching streams were found, keep the originals
+      streamsToKeep.set(
+        streamType,
+        originalStreams.filter((s) => s.codec_type === streamType),
+      );
+      args.jobLog(
+        `No matching streams were found for codec type ${streamType}, keeping the originals.`,
+      );
+    } else {
+      streamsToKeep.set(streamType, filteredStreams);
+      doProcessing = true;
+    }
+  });
+
+  if (doProcessing) {
+    // eslint-disable-next-line no-param-reassign
+    args.variables.ffmpegCommand.shouldProcess = true;
+
+    const outputStreams = Array.from(streamsToKeep)
+      .map(([, streams]) => streams)
+      .flat();
+    // eslint-disable-next-line no-param-reassign
+    args.variables.ffmpegCommand.streams = outputStreams;
+
+    // Set the first stream for each codec type as the default, clearing the rest
+    const seenStreamTypes: Set<string> = new Set();
+    const dispositionRemovalArgs: string[] = [];
+    const dispositionSetArgs: string[] = [];
+
+    outputStreams.forEach((stream, i) => {
+      if (seenStreamTypes.has(stream.codec_type)) {
+        dispositionRemovalArgs.push(`-disposition:${i}`, '0');
+      } else {
+        dispositionSetArgs.push(`-disposition:${i}`, 'default');
+        seenStreamTypes.add(stream.codec_type);
+      }
+    });
+
+    args.variables.ffmpegCommand.overallOuputArguments.push(
+      ...dispositionRemovalArgs,
+      ...dispositionSetArgs,
+    );
+  }
+
+  return {
+    outputFileObj: args.inputFileObj,
+    outputNumber: 1,
+    variables: args.variables,
+  };
+};
+export { details, plugin };
